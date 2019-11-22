f807d278ae2598e4422c6242dcd7fd0b68931816
guricchi08 <ahyar4y08@gmail.com>
2019-11-22 07:59:41 +0700

    Add project files.

---
+++ PA8_Revised/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Dim img As Graphics
+    Dim vertexList() As Vector3D
+
+    Dim sCenterX, sCenterY As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        img = PictureBox1.CreateGraphics
+        sCenterX = PictureBox1.Width / 2
+        sCenterY = PictureBox1.Height / 2
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        Label1.Text = "X: " + (e.X - sCenterX).ToString
+        Label2.Text = "Y: " + (-1 * (e.Y - sCenterY)).ToString
+    End Sub
+
+
+End Class
---
+++ PA8_Revised/Module1.vb
@@ -0,0 +1,68 @@
+﻿Module Module1
+    Class Point3D
+        Dim x, y, z As Double
+        Public Sub New()
+            x = 0.0
+            y = 0.0
+            z = 0.0
+        End Sub
+
+        Public Sub New(_x As Double, _y As Double, _z As Double)
+            x = _x
+            y = _y
+            z = _z
+        End Sub
+
+        Public Function ToVector() As Vector3D
+            Return New Vector3D(x, y, z)
+        End Function
+    End Class
+
+    Class Vector3D
+        Dim x, y, z As Double
+
+        Public Sub New()
+            x = 0.0
+            y = 0.0
+            z = 0.0
+        End Sub
+
+        Public Sub New(_x As Double, _y As Double, _z As Double)
+            x = _x
+            y = _y
+            z = _z
+        End Sub
+
+        Public Function DotProduct(v As Vector3D) As Double
+            Return (Me.x * v.x + Me.y * v.y + Me.z * v.z)
+        End Function
+
+        Public Function GetMagnitude() As Double
+            Return Math.Sqrt(DotProduct(Me))
+        End Function
+
+        Public Function CrossProduct(v As Vector3D) As Vector3D
+            Dim vOut As New Vector3D With {
+                .x = Me.y * v.z - Me.z * v.y,
+                .y = Me.z * v.x - Me.x * v.z,
+                .z = Me.x * v.y - Me.y * v.x
+            }
+
+            Return vOut
+        End Function
+
+        Public Function Normalize() As Vector3D
+            Dim mag As Double = Me.GetMagnitude
+
+            If mag > 0 Then
+                Me.x /= mag
+                Me.y /= mag
+                Me.z /= mag
+            End If
+
+            Return Me
+        End Function
+
+
+    End Class
+End Module
